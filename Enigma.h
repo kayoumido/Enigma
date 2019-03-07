b075f0a01963b78f46a736dcbf6ed5d449ac5e76
Doran <dorankayoumi@gmail.com>
2019-03-07 16:10:24 +0100

    Add base for enigma class

--- Enigma.h
+++ Enigma.h
@@ -5,9 +5,30 @@
 #ifndef LABO2_ENIGMA_H
 #define LABO2_ENIGMA_H
 
+#include <string>
+
+#include "Rotor.h"
+#include "Reflector.h"
+
+enum class RotorPosition {LEFT, RIGHT, MIDDLE};
 
 class Enigma {
+public:
+    Enigma(const Rotor &LEFT_ROTOR, const Rotor &MIDDLE_ROTOR, const Rotor &RIGHT_ROTOR, const Reflector &REFLECTOR);
 
+    void changeReflector(const Reflector &NEW_REFLECTOR) const;
+    void changeRotor(const RotorPosition &ROTOR_TO_CHANGE, const Rotor &NEW_ROTOR) const;
+    void changeRotorPosition(const RotorPosition &ROTOR_1, const RotorPosition &ROTOR_2) const;
+    std::string convertString(const std::string &STRING_TO_CONVERT) const;
+    
+private:
+    Rotor leftRotor;
+    Rotor middleRotor;
+    Rotor rightRotor;
+
+    Reflector reflector;
+
+    char convertChar(char CHAR_TO_CONVERT) const;
 };
 
 
