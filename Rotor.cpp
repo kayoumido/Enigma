1c2a5ca7eb9a92353feecb93e123822ff0a60803
Doran <dorankayoumi@gmail.com>
2019-03-10 16:30:02 +0100

    Add base for Rotor

--- Rotor.cpp
+++ Rotor.cpp
@@ -3,3 +3,39 @@
 //
 
 #include "Rotor.h"
+
+using namespace std;
+
+const string Rotor::DEFAULT_WIRING[] = {
+    "EKMFLGDQVZNTOWYHXUSPAIBRCJ",
+    "AJDKSIRUXBLHWTMCQGZNPYFVOE",
+    "BDFHJLCPRTXVZNYEIWGAKMUSQO",
+    "ESOVPZJAYQUIRHXLNFTGKDCMWB",
+    "VZBRGITYUPSDNHLXAWMJQOFECK"
+};
+
+string Rotor::getWiring(const string &ID) {
+
+    if (ID == "I") return Rotor::DEFAULT_WIRING[0];
+
+    if (ID == "II") return Rotor::DEFAULT_WIRING[1];
+
+    if (ID == "III") return Rotor::DEFAULT_WIRING[2];
+
+    if (ID == "IV") return Rotor::DEFAULT_WIRING[3];
+
+    if (ID == "V") return Rotor::DEFAULT_WIRING[4];
+
+}
+
+Rotor::Rotor(const string &ID, const string &NOTCH, char position) :
+    wiring(Rotor::getWiring(ID)), notch(NOTCH), position(position) {}
+
+Rotor &Rotor::operator=(const Rotor &ROTOR) {
+
+    this->notch = ROTOR.notch;
+    this->wiring = ROTOR.wiring;
+    this->position = ROTOR.position;
+
+    return *this;
+}
--- Rotor.h
+++ Rotor.h
@@ -5,9 +5,22 @@
 #ifndef LABO2_ROTOR_H
 #define LABO2_ROTOR_H
 
+#include <string>
 
 class Rotor {
+    const static std::string DEFAULT_WIRING[];
+    std::string static getWiring(const std::string &ID);
 
+public:
+
+    Rotor(const std::string &ID, const std::string &NOTCH, char position);
+
+    Rotor& operator=(const Rotor& ROTOR);
+private:
+    std::string wiring;
+    std::string notch;
+    char position;
+    const std::string ENTRY = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"; // put static?
 };
 
 
