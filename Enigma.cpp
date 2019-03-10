3410faec0e52438db78167cd794beb5f78a92807
Doran <dorankayoumi@gmail.com>
2019-03-10 16:30:41 +0100

    Add base for enigma machine

--- Enigma.cpp
+++ Enigma.cpp
@@ -3,3 +3,34 @@
 //
 
 #include "Enigma.h"
+
+Enigma::Enigma(const Rotor &LEFT_ROTOR, const Rotor &MIDDLE_ROTOR, const Rotor &RIGHT_ROTOR,
+               const Reflector &REFLECTOR) : leftRotor(LEFT_ROTOR), middleRotor(MIDDLE_ROTOR), rightRotor(RIGHT_ROTOR),
+                                             reflector(REFLECTOR) {}
+
+void Enigma::changeRotor(const RotorPosition &ROTOR_TO_CHANGE, const Rotor &NEW_ROTOR) {
+    switch (ROTOR_TO_CHANGE) {
+        case RotorPosition::LEFT:
+            this->leftRotor = NEW_ROTOR;
+            break;
+        case RotorPosition::MIDDLE:
+            this->middleRotor = NEW_ROTOR;
+            break;
+        case RotorPosition::RIGHT:
+            this->rightRotor = NEW_ROTOR;
+            break;
+    }
+}
+
+void Enigma::changeReflector(const Reflector &NEW_REFLECTOR) {
+    this->reflector = NEW_REFLECTOR;
+}
+
+void Enigma::changeRotorPosition(const RotorPosition &ROTOR_1, const RotorPosition &ROTOR_2) {
+
+}
+
+char Enigma::convert(const char toConvert) const {
+    return 0;
+}
+
